adbe82233ba101b95b015bcfe2014f89bbceb2d5
truedigital <aldane.barrett@mymona.uwi.edu>
2024-09-18 20:25:34 -0500

    Create login form

--- Client/src/pages/auth-view/login.jsx
+++ Client/src/pages/auth-view/login.jsx
@@ -1,11 +1,41 @@
+import CommonForm from "@/components/common-component/form";
+import { loginFormControls } from "@/components/config-component";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function AuthLogin()
-{
-    return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
+const initialState = {
+  userName: null,
+  password: "",
+};
+function AuthLogin() {
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit() {}
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+          Login
+        </h1>
+        <p className="mt-4 text-sm text-gray-600">
+          Dont have an account?
+          <Link
+            className="font-medium ml-1 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        buttonText={"Sign Up"}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 }
 
 export default AuthLogin;
