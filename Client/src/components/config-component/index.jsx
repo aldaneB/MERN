ff9cfd666c3799bcd134a1a6beaf959d9a8a5ba7
truedigital <aldane.barrett@mymona.uwi.edu>
2024-09-18 20:24:58 -0500

    Create form control

---
+++ Client/src/components/config-component/index.jsx
@@ -0,0 +1,42 @@
+/**This file will be used to configure different form views */
+
+export const registerFormControls = [
+  {
+    name: "username",
+    label: "Username",
+    placeholder: "Enter username",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter password",
+    componentType: "input",
+    type: "password",
+  },
+];
+
+export const loginFormControls = [
+  {
+    name: "username",
+    label: "Username",
+    placeholder: "Enter username",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter password",
+    componentType: "input",
+    type: "password",
+  },
+];
